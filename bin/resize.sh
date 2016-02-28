e34aa91add36b43661f27b997ff2e1cd7259ebb8
Alex Howansky <alex.howansky@gmail.com>
2016-02-28 18:41:18 +0000

    Added resize script.

---
+++ bin/resize.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+STAGE_DIR=${SCRIPT_DIR}/../staging
+PHOTO_DIR=${SCRIPT_DIR}/../public/photos
+
+cd ${STAGE_DIR} || exit
+for FILE in $(find . -mindepth 2 -maxdepth 2 -type f -name "*.jpg")
+do
+    GALLERY=$(echo ${FILE} | cut -d'/' -f2)
+    PHOTO=$(basename ${FILE})
+    if [ ! -d ${PHOTO_DIR}/${GALLERY} ]
+    then
+        mkdir ${PHOTO_DIR}/${GALLERY}
+    fi
+    if [ -f ${PHOTO_DIR}/${GALLERY}/${PHOTO} ]
+    then
+        echo "${GALLERY}/${PHOTO} ... skipped"
+    else
+        echo "${GALLERY}/${PHOTO} ... resizing"
+        cp ${FILE} ${PHOTO_DIR}/${GALLERY}/${PHOTO}
+        mogrify -resize 600x600 ${PHOTO_DIR}/${GALLERY}/${PHOTO}
+    fi
+done
