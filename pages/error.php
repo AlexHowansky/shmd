9bfc917bff2cb6ed2e4f4f41f8083ce9296d129d
Alex Howansky <alex.howansky@gmail.com>
2016-02-28 03:14:41 +0000

    Added menu to error page.

--- pages/error.php
+++ pages/error.php
@@ -1,4 +1,6 @@
 <?php
+$menu = ['/' => 'Home'];
+include '_menu.php';
 $e = $this->getLastError();
 if ($e === null) {
     header('HTTP/1.1 404 Page Not Found');
@@ -6,11 +8,13 @@ if ($e === null) {
     header('HTTP/1.1 500 Internal Server Error');
 }
 ?>
+<div style="padding: 25px;">
 <?php if ($e === null): ?>
-<h1>Page Not Found</h1>
+    <h1>Page Not Found</h1>
 <?php else: ?>
-<h1><?= $this->getLastError()->getMessage() ?></h1>
-<div>
+    <h1><?= $this->getLastError()->getMessage() ?></h1>
+    <div>
 <?= nl2br($this->getLastError()->getTraceAsString()) ?>
-</div>
+    </div>
 <?php endif; ?>
+</div>
