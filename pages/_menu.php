c890c6c07dcfb4cde902834774ad3a702026833d
Alex Howansky <alex.howansky@gmail.com>
2019-02-18 19:56:56 -0500

    Avoid warning.

--- pages/_menu.php
+++ pages/_menu.php
@@ -6,7 +6,7 @@
 <?php endif; ?>
     <div class="right menu">
         <div class="item">
-<?php if ($_COOKIE['lastSearch']): ?>
+<?php if (isset($_COOKIE['lastSearch']) === true): ?>
             <div class="ui left action icon input">
                 <a class="ui big black button" href="/search/<?= $_COOKIE['lastSearch'] ?>"><?= urldecode($_COOKIE['lastSearch']) ?></a>
 <?php else: ?>
