178208a6eb05841e4ecbebcb19a5d921439e6d23
Alex Howansky <alex.howansky@gmail.com>
2020-02-17 22:49:10 -0500

    Added receipt detail to order submission page.

--- pages/okorder.php
+++ pages/okorder.php
@@ -1,12 +1,42 @@
 <?php
+$order = $this->getOrder($this->getParam());
 $menu = ['/' => 'Home'];
-require_once '_menu.php';
+require_once '_order_menu.php';
 ?>
-<div class="ui relaxed list">
-    <div class="item">
-        <h1 class="ui centered header">Order Placed</h1>
-    </div>
-</div>
 <div class="ui raised container segment">
-    <a class="ui huge blue button" href="/">Continue</a>
+    <h1 class="ui centered header">Order Placed</h1>
+    <table class="ui celled table">
+        <thead>
+            <tr>
+                <th>Size</th>
+                <th class="right aligned">Quantity</th>
+                <th class="right aligned">Unit Price</th>
+                <th class="right aligned">Subtotal</th>
+            </tr>
+        </thead>
+        <tbody>
+<?php foreach ($order['quantity'] as $size => $quantity) :?>
+            <tr>
+                <td><?= $size ?></td>
+                <td class="right aligned"><?= $quantity ?></td>
+                <td class="right aligned"><?= money_format('%n', $this->getPriceForSize($size)) ?></td>
+                <td class="right aligned"><?= money_format('%n', $this->getPriceForSize($size) * $quantity) ?></td>
+            </tr>
+<?php endforeach; ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="4" class="right aligned">
+                    <h1 class="ui header">
+                        <?= money_format('%n', $order['total']) ?>
+                    </h1>
+                </th>
+            </tr>
+        </tfoot>
+    </table>
+    <div class="ui grid">
+        <div class="sixteen wide column right aligned">
+            <a href="/" class="ui huge blue button">Continue</a>
+        </div>
+    </div>
 </div>
