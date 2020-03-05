aa356eb28257f4039c3c5029161c5fa7e58dd6b4
Alex Howansky <alex.howansky@gmail.com>
2020-03-05 11:02:26 -0500

    Added hotFolderLog option.

--- pages/print.php
+++ pages/print.php
@@ -1,2 +1,18 @@
 <?php
-$this->printPhoto($this->getParam(0), urldecode($this->getParam(1)));
+$gallery = $this->getParam(0);
+$photo = urldecode($this->getParam(1));
+$this->printPhoto($gallery, $photo);
+if (empty($this->config['hotFolderLog']) === false) {
+    $result = file_put_contents(
+        $this->config['hotFolderLog'],
+        json_encode([
+            'timestamp' => time(),
+            'gallery' => $gallery,
+            'photo' => $photo,
+        ]) . "\n",
+        FILE_APPEND
+    );
+    if ($result === false) {
+        throw new \RuntimeException('Unable to write to hot folder log: ' . $this->config['hotFolderLog']);
+    }
+}
